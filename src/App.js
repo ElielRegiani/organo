dd52e8256bc493ef079f5b33f578e30dbef6443e
ElielRegiani <elielregiani@gmail.com>
2023-07-08 22:54:34 -0300

    Adding form and TextField

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Banner from './components/Banner/Banner';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Form />
+
     </div>
   );
 }
